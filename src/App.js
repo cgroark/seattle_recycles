9c2dceed17f90a4ca6267f989c5fe876fe6f2950
cgroark <colingroark@gmail.com>
2018-03-27 06:46:42 -0700

    added data button and date selection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,40 @@ import Recycling from './Recycling.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      recData: []
+    }
+
+  this.handleClick = this.handleClick.bind(this)
+  this.startYear = this.startYear.bind(this)
+
+  }
+
+  handleClick(e){
+    e.preventDefault();
+    let recApi = "https://performance.seattle.gov/resource/28jq-psn3.json?$$app_token=gPJkv0UyrUI43VB2spGv2uKxz"
+    fetch(recApi)
+      .then((response) => {
+        response.json().then((json) => {
+        this.setState({recData: json});
+      }).catch((err) => {
+        console.log("error found in parsing", err)
+      }); 
+      })
+  }
+  startYear(e){
+    e.preventDefault();
+    let years = []
+    let allData = this.state.recData.map(item => {
+      let year = item.year.slice(0,4)
+      years.push(year)
+      console.log(years)
+
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +44,37 @@ class App extends Component {
           <h1 className="App-title">Seattle Recycling Rates</h1>
         </header>
         <p className="App-intro">Compare Seattle Recycling rates for 2003-2015</p>
-        < Recycling />
+        <input type="button" value="Fetch Data" onClick={this.handleClick} />
+        <select className="drop-down" required onChange={this.startYear}>
+            <option  defaultValue="">Start Year</option>
+            <option  value="2003">2003</option>
+            <option  value="2004">2004</option>
+            <option  value="2005">2005</option>
+            <option  value="2006">2006</option>
+            <option  value="2007">2007</option>
+            <option  value="2008">2008</option>
+            <option  value="2008">2010</option>
+            <option  value="2008">2011</option>
+            <option  value="2008">2012</option>
+            <option  value="2008">2013</option>
+            <option  value="2008">2014</option>
+          </select>
+
+          <select className="drop-down" required onChange={this.endYear}>
+            <option  defaultValue="">End Year</option>
+            <option  value="2004">2004</option>
+            <option  value="2005">2005</option>
+            <option  value="2006">2006</option>
+            <option  value="2007">2007</option>
+            <option  value="2008">2008</option>
+            <option  value="2008">2010</option>
+            <option  value="2008">2011</option>
+            <option  value="2008">2012</option>
+            <option  value="2008">2013</option>
+            <option  value="2008">2014</option>
+            <option  value="2008">2015</option>
+          </select>
+        <Recycling recycle = {this.state.recData} />
       </div>
     );
   }
--- src/Recycling.js
+++ src/Recycling.js
@@ -3,30 +3,14 @@ import './App.css';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar } from 'recharts';
 
 class Recycling extends Component{
-	constructor(props){
-		super(props)
-		this.state={
-			recData: []
-		}
-	}
-	componentDidMount(){
 	
-		let recApi = "https://performance.seattle.gov/resource/28jq-psn3.json?$$app_token=gPJkv0UyrUI43VB2spGv2uKxz"
-		fetch(recApi)
-			.then((response) => {
-				return response.json()
-			}).then((json) => {
-				this.setState({recData: json});
-			}).catch((err) => {
-				console.log("error found in parsing", err)
-			});	
-	}
+
 	render(){
 		let years = []
 		let percent = []
-		const dataRate = []
-		const dataTonn = []
-		let allData = this.state.recData.map(item => {
+		let dataRate = []
+		let dataTonn = []
+		let allData = this.props.recycle.map(item => {
 			let eachRate = {}
 			let eachTonn = {}
 			let year = item.year.slice(0,4)
@@ -39,17 +23,8 @@ class Recycling extends Component{
 			eachTonn.year = year
 			eachTonn.Disposed = item.disposed 
 			eachTonn.Recycled = item.recycled 
-			dataTonn.push(eachTonn)
-
-			return(
-				<div className="rec_section">
-					<h3>Year: {item.year}</h3>
-					<h4>Rate: {item.recycle_rate}</h4>
-				</div>
-			)
+			dataTonn.push(eachTonn)	
 		})
-		console.log('dataaaa', dataRate, dataTonn)
-	
 		return(
 			<div>
 			<div className="line-chart">
@@ -65,17 +40,19 @@ class Recycling extends Component{
 				</LineChart>
 			</div>
 			<div className="bar-chart">
-			<h3>Waste disposed vs. recycled (tons) (%)</h3>
+			<h3>Waste disposed vs. recycled (tons)</h3>
 				<BarChart width={600} height={400} data={dataTonn} margin={{top: 20, right: 50, left: 50, bottom: 5}}>
 					<Bar dataKey="Disposed" stackId="a" fill="black" />
 					<Bar dataKey="Recycled" stackId="a" fill="#82ca9d" />
 			       	<XAxis dataKey="year"/>
 					<YAxis unit=" tons" ticks={[100000, 150000, 200000, 250000, 300000, 350000, 400000, 450000, 500000, 550000, 600000, 700000, 750000, 800000, 850000, 900000]} />
 			       	<CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-			       	<Tooltip/>
+			       	<Tooltip 
+			       	cursor={false}/>
 			       	<Legend />
 	      		</BarChart>
 	      		</div>
+	      		
 			</div>
 		)
 	}
